a89b0801e95bc05c69375cb01e8a099ab0636bcb
97868579 <97868579@qq.com>
2021-03-01 15:54:10 +0100

    first commit

---
+++ src/main/java/test_hashtag_wordcount/Wordcount_batch.java
@@ -0,0 +1,41 @@
+package test_hashtag_wordcount;
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.DataSet;
+import org.apache.flink.api.java.ExecutionEnvironment;
+//import org.apache.flink.api.java.operators.DataSource;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.util.Collector;
+
+//batch processing test
+public class Wordcount_batch {
+    public static void main(String[] args) throws Exception {
+        //创建运行环境
+        ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+        String inputpath = "D:\\hashtag-Flink\\src\\main\\resources\\test.txt";
+        DataSet<String> inputsource = env.readTextFile(inputpath);
+        //处理数据，使数据输出位一个二元组(word,1)进行统计
+        DataSet<Tuple2<String,Integer>> resultSet = inputsource.flatMap(new MyFlatMapper())
+                   .groupBy(0)//按照第一个位置的word分组
+                   .sum(1);//把第二个位置的数字求和
+
+        resultSet.print();
+
+
+    }
+    //自定义类，实现flatmapfunction接口，注意接口中的泛型
+    public static class MyFlatMapper implements FlatMapFunction<String, Tuple2<String,Integer>>{
+
+
+        public void flatMap(String s, Collector<Tuple2<String, Integer>> collector) throws Exception {
+            String[] words = s.split(" ");
+            //遍历所有word，包装成二元组进行输出
+            for(String str:words){
+                collector.collect(new Tuple2<String, Integer>(str,1));
+            }
+
+        }
+    }
+
+
+}
